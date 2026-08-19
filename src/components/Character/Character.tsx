38a57a7f290ce784921bcfb662fbd3e56ba0ce68
Frederic Nguyen Duy <frederic.nguyen-duy@se.com>
2026-08-19 23:38:53 +0700

    Refactored Character component

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,50 +1,22 @@
-import type { CSSProperties } from 'react'
 import defaultBodyUrl from '../../../assets/girl-body.png'
 import defaultFeetUrl from '../../../assets/girl-feet.png'
 import defaultHeadUrl from '../../../assets/girl-head.png'
 import type { CharacterChoice } from '../../types/CharacterChoice'
 import './Character.css'
 
-type CharacterStyle = CSSProperties & {
-  '--character-height': string
-  '--character-top': string
-  '--character-left': string
-  '--character-head-body-overlap': string
-  '--character-body-feet-overlap': string
-  '--character-head-padding-x': string
-  '--character-head-padding-y': string
-  '--character-body-padding-x': string
-  '--character-body-padding-y': string
-  '--character-feet-padding-x': string
-  '--character-feet-padding-y': string
-}
-
 const CHARACTER_LABEL = 'Character'
 const CHARACTER_HEAD_BODY_OVERLAP = '30%'
 const CHARACTER_BODY_FEET_OVERLAP = '17.333333%'
-const CHARACTER_PART_PADDING = '0px'
 
 function toPercentage(value: number): string {
   return `${value}%`
 }
 
-function getCharacterStyle(
-  size: number,
-  top: number,
-  left: number,
-): CharacterStyle {
+function getCharacterStyle(size: number, top: number, left: number) {
   return {
-    '--character-height': toPercentage(size),
-    '--character-top': toPercentage(top),
-    '--character-left': toPercentage(left),
-    '--character-head-body-overlap': CHARACTER_HEAD_BODY_OVERLAP,
-    '--character-body-feet-overlap': CHARACTER_BODY_FEET_OVERLAP,
-    '--character-head-padding-x': CHARACTER_PART_PADDING,
-    '--character-head-padding-y': CHARACTER_PART_PADDING,
-    '--character-body-padding-x': CHARACTER_PART_PADDING,
-    '--character-body-padding-y': CHARACTER_PART_PADDING,
-    '--character-feet-padding-x': CHARACTER_PART_PADDING,
-    '--character-feet-padding-y': CHARACTER_PART_PADDING,
+    height: toPercentage(size),
+    top: toPercentage(top),
+    left: toPercentage(left),
   }
 }
 
@@ -89,6 +61,7 @@ export function Character({
 
       <button
         className="character__part character__body"
+        style={{ marginTop: `-${CHARACTER_HEAD_BODY_OVERLAP}` }}
         type="button"
         aria-label={`Body option ${characterChoice.bodyIndex}`}
         onClick={onBodyClick}
@@ -98,6 +71,7 @@ export function Character({
 
       <button
         className="character__part character__feet"
+        style={{ marginTop: `-${CHARACTER_BODY_FEET_OVERLAP}` }}
         type="button"
         aria-label={`Feet option ${characterChoice.feetIndex}`}
         onClick={onFeetClick}
